04a71ab3b97e66c6eb37e743f0b2c95b933bc7f1
harry <housne@gmail.com>
2024-03-14 08:12:52 +0800

    init astro

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const articles = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		date: z.coerce.date(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { articles };
